c259116749ba503bb214100baea7ed72f14c3d21
Tom Seddon <modelb@bbcmicro.com>
2020-04-13 16:00:26 +0100

    Move key sym map flag into BeebKeymap.

--- src/b2/BeebKeymap.h
+++ src/b2/BeebKeymap.h
@@ -14,17 +14,27 @@ struct BeebKeymapTraits {
     static const int8_t TERMINATOR=-1;
 };
 
+// The mapping is SDL_Scancode<->BeebKey (for scancode mappings) or
+// SDL_Keycode+PCKeyModifier<->BeebKeySym (for keysym mappings). Use
+// IsKeySymMap to find out what sort of mapping this is.
+//
+// The keymap holds the keysym/scancode flag, but doesn't otherwise
+// distinguish between the two types of map.
+
 class BeebKeymap:
     public Keymap<BeebKeymapTraits>
 {
 public:
     BeebKeymap(std::string name,
-               bool key_sym_map);
+               bool is_key_sym_map);
+
+    bool IsKeySymMap() const;
 
     bool GetPreferShortcuts() const;
     void SetPreferShortcuts(bool prefer_shortcuts);
 protected:
 private:
+    bool m_is_key_sym_map=false;
     bool m_prefer_shortcuts=false;
 };
 
