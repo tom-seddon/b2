33027c7ed473419bdabb716d913cd171e28f5d91
Tom Seddon <modelb@bbcmicro.com>
2021-03-27 14:47:05 +0000

    Don't call VS2015 vsvars32.bat.
    Attempt to fix the "The build tools for v141 (Platform Toolset = 'v141')
    cannot be found" error.

--- etc/release/build_windows.bat
+++ etc/release/build_windows.bat
@@ -2,6 +2,6 @@
 
 @REM this sets up the path for the x86 tools, but it doesn't appear to
 @REM cause a problem when building for x64.
-@CALL "%VS140COMNTOOLS%\..\Tools\vsvars32.bat"
+@REM @CALL "%VS140COMNTOOLS%\..\Tools\vsvars32.bat"
 
 @msbuild %*
