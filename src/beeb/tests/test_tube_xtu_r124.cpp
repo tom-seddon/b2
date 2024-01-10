accedb5cf28a45aef006fdded90796c62f943b41
Tom Seddon <modelb@bbcmicro.com>
2024-01-10 22:59:00 +0000

    Reinstate test_tube_xtu_r124, which now passes.

--- src/beeb/tests/test_tube_xtu_r124.cpp
+++ src/beeb/tests/test_tube_xtu_r124.cpp
@@ -2,15 +2,15 @@
 #include "test_tube.h"
 
 int main() {
-    //TestBBCMicroArgs args = {};
-    //args.flags = TestBBCMicroFlags_ConfigureExTube;
-    //TestTube("test_tube_xtu_r124",
-    //         TestBBCMicroType_Master128MOS320WithExternal3MHz6502,
-    //         args,
-    //         BEEBLINK_VOLUME_PATH,
-    //         "4",
-    //         "R124",
-    //         0x800,
-    //         "OLD\r*SPOOL X.R124\r",
-    //         "*SPOOL\r");
+    TestBBCMicroArgs args = {};
+    args.flags = TestBBCMicroFlags_ConfigureExTube;
+    TestTube("test_tube_xtu_r124",
+             TestBBCMicroType_Master128MOS320WithExternal3MHz6502,
+             args,
+             BEEBLINK_VOLUME_PATH,
+             "4",
+             "R124",
+             0x800,
+             "OLD\r*SPOOL X.R124\r",
+             "*SPOOL\r");
 }
