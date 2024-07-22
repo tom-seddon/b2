tom-seddon/b2

commit 0952c3684d12fa26ac3c567c883ce5be76fef617

Fix Linux build warnings.

src/beeb/src/Trace.cpp
@@ -119,7 +119,7 @@ const TraceEventType Trace::SET_MAPPER_REGION_EVENT("_set_mapper_region", sizeof
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-// must be a POD type - it's allocated with malloc.
+// Allocated with malloc, so a bit of care needs to be taken.
 struct Trace::Chunk {
     struct Chunk *next;
     size_t size;
@@ -158,6 +158,7 @@ Trace::~Trace() {
     while (c) {
         Chunk *next = c->next;
 
+        static_assert(std::is_trivially_destructible<decltype(*c)>::value);
         free(c);
 
         c = next;
@@ -614,7 +615,7 @@ void *Trace::Alloc(CycleCount time, size_t n) {
             }
         }
 
-        memset(c, 0, sizeof *c);
+        new(c) Chunk;
         c->capacity = size;
         c->initial_time = c->last_time = time;
         c->initial_paging = m_paging;

src/b2/BeebConfig.inl
@@ -10,7 +10,7 @@
 // and it won't come back automatically.)
 
 #define ENAME BeebConfigFeatureFlag
-EBEGIN()
+EBEGIN_DERIVED(uint32_t)
 EPNV(MasterTurbo, 1 << 0)
 EPNV(6502SecondProcessor, 1 << 1)
 EPNV(MasterCompact, 1 << 2)

src/b2/TraceUI.cpp
@@ -220,7 +220,7 @@ static void DoTraceFlag(uint32_t *flags_seen,
     *flags_seen |= flag;
 
     if (!text) {
-        text = GetBBCMicroTraceFlagEnumName((int)flag);
+        text = GetBBCMicroTraceFlagEnumName(flag);
     }
 
     ImGuiIDPusher id_pusher(flag);
@@ -366,7 +366,7 @@ void TraceUI::DoImGui() {
 
         for (uint32_t i = 1; i != 0; i <<= 1) {
             if (!(flags_seen & i)) {
-                const char *name = GetBBCMicroTraceFlagEnumName((int)i);
+                const char *name = GetBBCMicroTraceFlagEnumName(i);
                 if (name[0] == '?') {
                     continue;
                 }

src/b2/debugger.cpp
@@ -3126,7 +3126,7 @@ class StackDebugWindow : public DebugUI {
 
                 M6502Word addr;
                 addr.b.l = value;
-                addr.b.h = value_dbp->bp.r[0x100 + (offset + 1 & 0xff)];
+                addr.b.h = value_dbp->bp.r[0x100 + ((offset + 1) & 0xff)];
 
                 ImGuiIDPusher pusher(offset);
 

src/b2/load_save.cpp
@@ -938,9 +938,9 @@ static bool FindBitIndexedFlagsMember(T *flags, rapidjson::Value *object, const 
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-static void SaveFlags(JSONWriter<StringStream> *writer, uint32_t flags, const char *(*get_name_fn)(int)) {
+static void SaveFlags(JSONWriter<StringStream> *writer, uint32_t flags, const char *(*get_name_fn)(uint32_t)) {
     for (uint32_t mask = 1; mask != 0; mask <<= 1) {
-        const char *name = (*get_name_fn)((int)mask);
+        const char *name = (*get_name_fn)(mask);
         if (name[0] == '?') {
             continue;
         }
@@ -956,7 +956,7 @@ static void SaveFlags(JSONWriter<StringStream> *writer, uint32_t flags, const ch
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-static bool FindFlagsMember(uint32_t *flags, rapidjson::Value *object, const char *key, const char *what, const char *(*get_name_fn)(int), Messages *msg) {
+static bool FindFlagsMember(uint32_t *flags, rapidjson::Value *object, const char *key, const char *what, const char *(*get_name_fn)(uint32_t), Messages *msg) {
     rapidjson::Value array;
     if (!FindArrayMember(&array, object, key, msg)) {
         return false;
@@ -971,7 +971,7 @@ static bool FindFlagsMember(uint32_t *flags, rapidjson::Value *object, const cha
             const char *flag_name = array[i].GetString();
 
             for (uint32_t mask = 1; mask != 0; mask <<= 1) {
-                const char *name = (*get_name_fn)((int)mask);
+                const char *name = (*get_name_fn)(mask);
                 if (name[0] == '?') {
                     continue;
                 }

src/beeb/include/beeb/BBCMicro.inl
@@ -15,7 +15,7 @@ EEND()
 //////////////////////////////////////////////////////////////////////////
 
 #define ENAME BBCMicroTraceFlag
-EBEGIN()
+EBEGIN_DERIVED(uint32_t)
 EPNV(6845, 1 << 0)
 EPNV(6845Scanlines, 1 << 1)
 EPNV(6845ScanlinesSeparators, 1 << 2)

src/beeb/include/beeb/DiscInterface.h
@@ -10,6 +10,7 @@ class DiscInterface;
 #include "roms.h"
 #include <string>
 #include <functional>
+#include <memory>
 
 #include <shared/enum_decl.h>
 #include "DiscInterface.inl"

src/beeb/include/beeb/SaveTrace.inl
@@ -1,5 +1,5 @@
 #define ENAME TraceOutputFlags
-EBEGIN()
+EBEGIN_DERIVED(uint32_t)
 // If set, include register names in output (takes up more columns...)
 EPNV(RegisterNames, 1)
 

commit ecb53f2f59be118fdca546e2a66c2a5306a737aa

Initialize trace chunks.
(Problem introduced by 0952c3684d12fa26ac3c567c883ce5be76fef617)

src/beeb/src/Trace.cpp
@@ -119,18 +119,17 @@ const TraceEventType Trace::SET_MAPPER_REGION_EVENT("_set_mapper_region", sizeof
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-// Allocated with malloc, so a bit of care needs to be taken.
 struct Trace::Chunk {
-    struct Chunk *next;
-    size_t size;
-    size_t capacity;
-    size_t num_events;
+    struct Chunk *next = nullptr;
+    size_t size = 0;
+    size_t capacity = 0;
+    size_t num_events = 0;
 
-    CycleCount initial_time;
-    CycleCount last_time;
+    CycleCount initial_time = {};
+    CycleCount last_time = {};
 
     PagingState initial_paging;
-    bool initial_parasite_boot_mode;
+    bool initial_parasite_boot_mode = false;
 };
 
 //////////////////////////////////////////////////////////////////////////
@@ -615,7 +614,7 @@ void *Trace::Alloc(CycleCount time, size_t n) {
             }
         }
 
-        new(c) Chunk;
+        new (c) Chunk;
         c->capacity = size;
         c->initial_time = c->last_time = time;
         c->initial_paging = m_paging;