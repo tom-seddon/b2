a0861e85156e9f4a4ce3d0286ede938c9edf9d7b
Tom Seddon <modelb@bbcmicro.com>
2024-08-17 15:22:19 +0100

    Use 1 .yml fil for GH Actions. Add Windows test. [skip-appveyor]

--- etc/release/build_windows.bat
+++ etc/release/build_windows.bat
@@ -1,5 +1,16 @@
 @SETLOCAL
 
+@REM Work around msbuild getting confused by the path environment
+@REM variable not being spelled PATH (case-sensitive). See
+@REM https://github.com/dotnet/msbuild/issues/5726
+@REM
+@REM 
+
+@set _PATH=%PATH%
+@set PATH=
+@set PATH=%_PATH%
+@set _PATH=
+
 @REM this sets up the path for the x86 tools, but it doesn't appear to
 @REM cause a problem when building for x64.
 @REM @CALL "%VS140COMNTOOLS%\..\Tools\vsvars32.bat"
