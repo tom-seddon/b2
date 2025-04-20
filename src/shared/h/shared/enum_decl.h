81b2007a98724e6f8421ae65c52ddb925a141abe
Tom Seddon <->
2025-04-20 13:29:33 +0100

    Tidy up the enum base type (assuming gcc+clang accepts it).

--- src/shared/h/shared/enum_decl.h
+++ src/shared/h/shared/enum_decl.h
@@ -6,11 +6,16 @@
 #define EPREFIX
 #endif
 
-// The ENAME##BaseType thing is a bit ugly, but at this point the actual type
-// isn't available. And when an ordinary enum, it can't be forward declared.
 #define EBEGIN__BODY(COLON_BASE_TYPE, BASE_TYPE)                               \
-    typedef BASE_TYPE CONCAT2(ENAME, BaseType);                                \
+    enum ENAME COLON_BASE_TYPE;                                                \
+                                                                               \
+    template <>                                                                \
+    struct EnumBaseType<ENAME> {                                               \
+        typedef BASE_TYPE Type;                                                \
+    };                                                                         \
+                                                                               \
     EPREFIX const char *UNUSED CONCAT3(Get, ENAME, EnumName)(BASE_TYPE value); \
+                                                                               \
     enum ENAME COLON_BASE_TYPE {
 
 #define EBEGIN() EBEGIN__BODY(, int)
@@ -33,20 +38,12 @@
     typedef enum ENAME ENAME;                                                 \
                                                                               \
     template <>                                                               \
-    struct EnumBaseType<ENAME> {                                              \
-        typedef CONCAT2(ENAME, BaseType) Type;                                \
-    };                                                                        \
-                                                                              \
-    template <>                                                               \
     struct EnumTraits<ENAME> {                                                \
         typedef const char *(*GetNameFn)(typename EnumBaseType<ENAME>::Type); \
         static const GetNameFn GET_NAME_FN;                                   \
         static const char NAME[];                                             \
     };
 
-//#define EOVERLOAD() const char *GetEnumValueName(ENAME value);
-
 #define NBEGIN(NAME) EPREFIX const char *UNUSED CONCAT3(Get, NAME, EnumName)(ENAME value);
 #define NEND()
 #define NN(NAME)
-//#define NOVERLOAD() const char *GetEnumValueName(ENAME value);
