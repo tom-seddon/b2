7733913e93e3605b028322a2476be75fd5ca591f
Tom Seddon <tom@trlinux>
2025-10-11 16:28:29 +0100

    Use pushwarn_case_fallthrough.h instead of GCC-specific pragma.

--- experimental/test_ffmpeg/test_ffmpeg.cpp
+++ experimental/test_ffmpeg/test_ffmpeg.cpp
@@ -87,9 +87,10 @@ static void PrintLastAVResult(const TestFailArgs *tfa) {
 #ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wsign-conversion"
-#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
 #endif
+#include <shared/pushwarn_case_fallthrough.h>
 #include "stb_image.h"
+#include <shared/popwarn.h>
 #ifdef __GNUC__
 #pragma GCC diagnostic pop
 #endif
