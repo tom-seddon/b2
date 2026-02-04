358d408584d5d9506766205019ab20575455d6a3
Tom Seddon <tom@ttr>
2026-02-04 17:07:25 +0000

    Fix Windows non-Debug builds.

--- src/b2/b2.cpp
+++ src/b2/b2.cpp
@@ -104,30 +104,6 @@ static SDL_threadID g_main_thread_id = 0;
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-#if SYSTEM_WINDOWS
-#if BUILD_TYPE_Final || BUILD_TYPE_RelWithDebInfo
-
-// Seems like there's no good way to do this from CMake:
-// http://stackoverflow.com/questions/8054734/
-//
-// But this here is simple enough.
-//
-// (There's no need to do anything about the entry point; the SDL2main stuff
-// already ensures that the program can work either way.)
-//
-// (One oddity: Visual Studio seems to examine the project settings to decide
-// whether to launch its own console window with the post exit
-// press-return-to-exit prompt. So the RelWithDebInfo build gets a console
-// window when run from Visual Studio, even though it's not actually a console
-// program.)
-#pragma comment(linker, "/SUBSYSTEM:WINDOWS")
-
-#endif
-#endif
-
-//////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
-
 #if BUILD_TYPE_Debug
 #define ENABLE_FAIL_STARTUP 1
 #else
--- src/b2/b2_app.cpp
+++ src/b2/b2_app.cpp
@@ -1,6 +1,33 @@
-// ...
 #include <shared/system.h>
 #include "b2.h"
+#include <SDL.h>
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+#if SYSTEM_WINDOWS
+#if BUILD_TYPE_Final || BUILD_TYPE_RelWithDebInfo
+
+// Seems like there's no good way to do this from CMake:
+// http://stackoverflow.com/questions/8054734/
+//
+// But this here is simple enough.
+//
+// (There's no need to do anything about the entry point; the SDL2main stuff
+// already ensures that the program can work either way.)
+//
+// (One oddity: Visual Studio seems to examine the project settings to decide
+// whether to launch its own console window with the post exit
+// press-return-to-exit prompt. So the RelWithDebInfo build gets a console
+// window when run from Visual Studio, even though it's not actually a console
+// program.)
+#pragma comment(linker, "/SUBSYSTEM:WINDOWS")
+
+#endif
+#endif
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char *argv[]) {
     return b2_main(argc, argv);
